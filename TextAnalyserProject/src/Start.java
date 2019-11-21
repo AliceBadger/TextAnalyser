e8739160ece8196f5b42455a62441ab31670cfb8
Alicja Trun <alicjatrun@gmail.com>
2019-11-21 22:39:54 +0100

    #task0 - input control initialization

---
+++ TextAnalyserProject/src/Start.java
@@ -0,0 +1,8 @@
+import  Controllers.*;
+
+class Start {
+    public static void main(String[] args) {
+        InputController inputController = new InputController();
+        inputController.initialize();
+    }
+}
